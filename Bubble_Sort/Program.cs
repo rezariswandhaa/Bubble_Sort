d8a83d4ac215167ac98288f004696bbdcf6ada8b
EMREZA <EMREZA@LAPTOP-2K069FQQ>
2022-10-06 17:19:55 +0700

    Add project files.

---
+++ Bubble_Sort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class program
+    {
+        private int[] a = new int[20];
+
+        private int n;
+
+        private void read()
+        {
+            while (true)
+            {
+                Console.Write("Masukan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------")
+        }
+    }
+}
